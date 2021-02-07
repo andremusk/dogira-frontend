7905ec4ac8e2ff737afe990e1411a35684b2b4de
andremusk <andremusk@protonmail.com>
2021-02-06 23:38:50 -0800

    footer disclaimer and disclosure as well as update on tokenomics percentages

--- assets/js/js-functions.js
+++ assets/js/js-functions.js
@@ -70,3 +70,37 @@ function scrollFunction() {
         document.getElementById("about-image").style.borderRadius = "0%";
     }
 }
+let content = document.createElement("p")
+let button1 = document.getElementById("disclaimer")
+let button2 = document.getElementById("disclosure")
+
+
+function renderDisclaimer(){
+    let render = document.getElementById("render")
+    
+    button1.addEventListener("click", () => {
+        button1.style.backgroundColor = "rgb(240, 159, 19)"
+        button1.style.color = "black"
+        button2.style.backgroundColor = "black"
+        button2.style.color = "white"
+        render.append(content)
+        content.innerText = `The information provided on this website does not constitute investment advice, financial advice, trading advice, or any other sort of advice and you should not treat any of the website's content as such. The DOGIRIA team does not recommend that any cryptocurrency should be bought, sold, or held by you. Do conduct your own due diligence and consult your financial advisor before making any investment decisions. By purchasing DOGIRIA, you agree that you are not purchasing a security or investment and you agree to hold the team harmless and not liable for any losses or taxes you may incur. You also agree that the team is presenting the token "as is" and is not required to provide any support or services. You should have no expectation of any form from DOGIRIA and its team. Although DOGIRIA is an EXPERIMENTAL token for social experiment and not a digital currency, the team strongly recommends that United States persons do not purchase it because the team cannot ensure compliance with United states regulations. Always make sure that you are in compliance with your local laws and regulations before you make any purchase.`
+
+    })
+}
+function renderDisclosure(){
+    let render = document.getElementById("render")
+    button2.addEventListener("click", () => {
+        button2.style.backgroundColor = "rgb(240, 159, 19)"
+        button2.style.color = "black"
+        button1.style.backgroundColor = "black"
+        button1.style.color = "white"
+        render.append(content)
+        content.id = "rendered-content"
+        content.innerText = `Please note there are always risks associated with smart-contracts. Please use at your own risk. DOGIRIA Token is not a registered broker, analyst or investment advisor. Everything that we provide on this site is purely for guidance, informational and educational purposes. All information contained herein should be independently verified and confirmed. We do not accept any liability for any loss or damage whatsoever caused in reliance upon such information or services.
+        Please be aware of the risks involved with any trading done in any financial market. Do not trade with money that you cannot afford to lose. When in doubt, you should consult a qualified financial advisor before making any investment decisions.`
+    })
+}
+
+renderDisclaimer()
+renderDisclosure()
